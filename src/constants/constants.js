b6eb4a933f5999323b423e00f029c22f8d83acce
gokul <gokulraj9946@gmail.com>
2024-02-01 10:18:38 +0000

    Initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY="a28a1003ee22115a52ffa86cc16c213c";
+export const imageUrl= 'https://image.tmdb.org/t/p/original';
+const apiRequest ='https://api.themoviedb.org/3/movie/460465/videos?api_key=a28a1003ee22115a52ffa86cc16c213c&language=en-US';
